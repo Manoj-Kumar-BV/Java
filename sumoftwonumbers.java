b9b87ef9699995f161efa8fd9e5ede67dd2c5835
Manoj Kumar BV <135972453+Manoj-Kumar-BV@users.noreply.github.com>
2024-11-06 00:05:27 +0530

    sumoftwonumbers

---
+++ sumoftwonumbers.java
@@ -0,0 +1,8 @@
+public class sumoftwonumbers {
+    public static void main(String args[]){
+        int a=10, b=5, c;
+        c=a+b;
+        System.out.println("Sum of Two Numbers");
+        System.out.println("Sum of : "+a+ " & " +b+ " is = " +c);
+    }
+}
