b0183aa66baced8d6087742444a5a4bfc4f992a9
Manoj Kumar BV <135972453+Manoj-Kumar-BV@users.noreply.github.com>
2024-11-07 23:05:27 +0530

    Assign - 2

---
+++ Assignments/assgn2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class assgn2 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the side of a square");
+        float side = sc.nextFloat();
+
+        float aos = side*side;
+
+        System.out.println("The area of a square is : "+aos);
+    }
+}
