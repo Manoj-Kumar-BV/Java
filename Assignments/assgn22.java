8e34b44252372c62d965d59f14321cbbb94b2106
Manoj Kumar BV <135972453+Manoj-Kumar-BV@users.noreply.github.com>
2024-12-11 00:56:23 +0530

    Assign - 22

---
+++ Assignments/assgn22.java
@@ -0,0 +1,30 @@
+public class assgn22 {
+    public static void selectionSort(int arr[]){
+        for(int i=0; i<arr.length-1; i++){
+            int mini = i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[mini] < arr[j]){
+                    mini = j;
+                }
+            }
+            int temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void printArray(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {3,6,2,1,8,7,4,5,3,1};
+
+        selectionSort(arr);
+
+        printArray(arr);
+    }
+}
