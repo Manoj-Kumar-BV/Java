a645c5dd2c3522392988cb58983669130f3bd87c
Manoj Kumar BV <135972453+Manoj-Kumar-BV@users.noreply.github.com>
2024-12-12 10:38:37 +0530

    Lt - 169(Majority Element)

---
+++ LeetCode/0169.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int start = 0;
+        int end = nums.length;
+        while(start <= end){
+            int mid = (start+end)/2;
+            return nums[mid];
+        }
+        return 0;
+    }
+}
