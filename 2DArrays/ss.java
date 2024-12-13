84ec75e22bb988ca3d6c108ce86c12c527691064
Manoj Kumar BV <135972453+Manoj-Kumar-BV@users.noreply.github.com>
2024-12-14 00:36:50 +0530

    Staircase Searching (On Considering first Column and last Row Element as Initial Element)

--- 2DArrays/ss.java
+++ 2DArrays/ss.java
@@ -1,26 +1,41 @@
 public class ss {
     public static boolean staircaseSearching(int matrix[][], int key){
-        int row = 0, col = matrix[0].length-1;
+    //     int row = 0, col = matrix[0].length-1;
 
-        while(row < matrix.length && col >= 0){
+    //     while(row < matrix.length && col >= 0){
+    //         if(matrix[row][col] == key){
+    //             System.out.println("Key Found at cell ("+row+","+col+")");
+    //             return true;
+    //         }
+    //         else if(key < matrix[row][col]){
+    //             col--;
+    //         }
+    //         else{
+    //             row++;
+    //         }
+    //     }
+    //     System.out.println("Key Not found!");
+    //     return false;
+        int row = matrix.length-1, col = 0;
+        while(col < matrix[0].length && row >= 0){
             if(matrix[row][col] == key){
-                System.out.println("Key Found at cell ("+row+","+col+")");
+                System.out.println("Key found at cell "+row+","+col);
                 return true;
             }
             else if(key < matrix[row][col]){
-                col--;
+                row--;
             }
             else{
-                row++;
+                col++;
             }
         }
-        System.out.println("Key Not found!");
+        System.out.println("Key not Found!");
         return false;
     }
 
     public static void main(String[] args) {
         int mat[][] = {{10,20,30,40},{15,25,35,45},{27,29,37,48},{32,33,39,50}};
 
-        staircaseSearching(mat, 100);
+        staircaseSearching(mat, 40);
     }
 }
