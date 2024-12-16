281b0df0ea04a2a1dda9786c8ee77cee3bb6d61a
Manoj Kumar BV <135972453+Manoj-Kumar-BV@users.noreply.github.com>
2024-12-16 14:20:10 +0530

    String Builder

---
+++ Strings/sb.java
@@ -0,0 +1,9 @@
+public class sb {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("");
+        for(char ch = 'a'; ch <= 'z'; ch++){
+            sb = sb.append(ch);
+        }
+        System.out.println(sb);
+    }
+}
