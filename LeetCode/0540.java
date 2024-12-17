2c6f13fec4238e0eb356a708ac152a109e32da4a
Manoj Kumar BV <135972453+Manoj-Kumar-BV@users.noreply.github.com>
2024-12-18 01:37:54 +0530

    Lt - 540(Single Element in a Sorted Array)

--- LeetCode/0540.java
+++ LeetCode/0540.java
@@ -24,3 +24,23 @@
 //     }
 // }
 //This is Brute Force which gives O(N) TC
+
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int low =0, high = nums.length-1;
+
+        while(low < high){
+            int mid = (low + high)/2;
+
+            boolean isEven = (mid%2==0);
+
+            if((isEven && nums[mid] == nums[mid+1]) || (!isEven && nums[mid] == nums[mid-1])){
+                low = mid+1;
+            }else{
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
+}
