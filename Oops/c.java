2f07b42fff1572a1148fd6ba8cbd156036644203
Manoj Kumar BV <135972453+Manoj-Kumar-BV@users.noreply.github.com>
2024-12-26 01:16:54 +0530

    Constructor

---
+++ Oops/c.java
@@ -0,0 +1,14 @@
+public class c {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+    }
+}
+
+class Student{
+    String name;
+    String roll;
+
+    Student(){
+        System.out.println("Comnstructor is called...");
+    }
+}
