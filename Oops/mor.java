b8a5fa6dbe4dc878981abd93f3f492a6bc549b61
Manoj Kumar BV <135972453+Manoj-Kumar-BV@users.noreply.github.com>
2024-12-28 23:47:37 +0530

    Method Overriding
    The child class object will be executed

---
+++ Oops/mor.java
@@ -0,0 +1,18 @@
+public class mor {
+    public static void main(String[] args) {
+        Deer d = new Deer();
+        d.eat();
+    }
+}
+
+class Animal{
+    void eat(){
+        System.out.println("Eats Anything");
+    }
+}
+
+class Deer extends Animal{
+    void eat(){
+        System.out.println("Eats Grass");
+    }
+}
