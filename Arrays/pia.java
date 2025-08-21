bbad32ff5893cc1b93a2fac37bec360dce4ac69b
Manoj Kumar BV <manojkumar69bv@gmail.com>
2025-08-21 23:01:05 +0530

    Pair in array

--- Arrays/pia.java
+++ Arrays/pia.java
@@ -1,9 +1,8 @@
 public class pia {
     public static void pairs(int arr[]){
-        for(int i =0; i<arr.length; i++){
-            int current = arr[i];
-            for(int j = i+1; j<arr.length; j++){
-                System.out.print("( "+current+" , "+arr[j]+" )");
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                System.out.print("("+arr[i]+", "+arr[j]+")");
             }
             System.out.println();
         }
