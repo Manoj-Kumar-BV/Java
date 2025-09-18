b7ede353b2fb23b7584bf06db4b5f0569c746222
Manoj Kumar BV <manojkumar69bv@gmail.com>
2025-09-18 18:45:54 +0530

    Add at the given index

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -35,6 +35,22 @@ public class LinkedList {
         tail = newNode;
     }
 
+    public void addatindex(int index, int data){
+        Node newNode = new Node(data);
+        if(index == 0){
+            addFirst(data);
+            return;
+        }
+        Node temp = head;
+        int i = 0;
+        while(i < index - 1){
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+    
     public void printll(){
         Node temp = head;
         while(temp != null){
