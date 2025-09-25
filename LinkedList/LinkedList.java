6e129a1f319f2da87d2c456f77d967c173c7da8f
Manoj Kumar BV <manojkumar69bv@gmail.com>
2025-09-25 10:00:04 +0530

    Updates new method

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -118,6 +118,41 @@ public class LinkedList {
         }
         return -1;
     }
+
+    public int recSearch(int key){
+        return helper(head, key);
+    }
+
+    public int helper(Node head, int key){
+        if(head == null){
+            return -1;
+        }
+
+        if(head.data == key){
+            return 0;
+        }
+
+        int idx = helper(head.next, key);
+        if(idx == -1){
+            return -1;
+        }
+        return idx+1;
+    }
+
+    public void reverse(){
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        head = prev;
+    }
     public static void main(String[] args) {
         LinkedList sll = new LinkedList();
         sll.addFirst(1);
